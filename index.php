469a720f120e321d714afb4cfad6e439b2872a05
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2024-08-20 14:35:11 -0500

    Crear structura basica de archivos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boda Lilibeth y Paul</title>
+    <link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./css/movil.css" media="screen and (min-width: 320px)">
+    <link rel="stylesheet" href="./css/tablet.css" media="screen and (min-width: 768px)">
+    <link rel="stylesheet" href="./css/portatil.css" media="screen and (min-width: 1200px)">
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="seccion presentacion"></section>
+        <section class="seccion informacion"></section>
+        <section class="seccion detalles"></section>
+    </main>
+    <footer></footer>
+    <script src="./app.js"></script>
+</body>
+</html>
