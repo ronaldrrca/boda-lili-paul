4daf2a3e1c58096a1a948ee644a3ca76229f08aa
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2024-08-20 16:24:19 -0500

    Agregar la seccion -presentacion

--- index.php
+++ index.php
@@ -14,9 +14,17 @@
     <main>
         <img class="main__fixedImage main__fixedImage__left" src="./assets/rama_de_flores_left.png" alt="rama de flores">
         <img class="main__fixedImage main__fixedImage__right" src="./assets/rama_de_flores_right.png" alt="rama de flores">
-        <section class="seccion presentacion"></section>
-        <section class="seccion informacion"></section>
+        <section class="seccion presentacion">
+            <img class="presentacion__logoNovios" src="./assets/logo.png" alt="logo de las iniciales de los novios entrelazadas con un corazon">
+            <h1 class="presentacion__nombresNovios">Lilibeth y Paul</h1>
+            <audio class="presentacion__reproductorDeAudio" controls loop>
+                <source src="./assets/la-boda.mp3" type="audio/mpeg">
+                Tu navegador no soporta este componente de audio.
+            </audio>
+            <img class="presentacion__imagenPrincipal" src="./assets/foto__principal.webp" alt="novios caminando por la playa">
+        </section>
         <section class="seccion detalles"></section>
+        <section class="seccion informacion"></section>
     </main>
     <footer></footer>
     <script src="./app.js"></script>
