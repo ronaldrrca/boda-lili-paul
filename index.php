50b3aac862480be4ee75d94c442f2e99902b4cd2
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2024-08-23 12:07:35 -0500

    Agregar el footer

--- index.php
+++ index.php
@@ -72,18 +72,55 @@
             <div class="informacion__momentos">
                 <div class="informacion__momentos__ceremonia">
                     <img class="informacion__iconoImagen" src="./assets/iglesia.svg" alt="iglesia">
-                    <span>Ceremonia Católica</span>
+                    <h2>Ceremonia Católica</h2>
                     <span>6:00pm</span>
+                    <span class="informacion__momentos__lugar">Catedral Metropolitana <br>de Barranquilla</span>
+                    <div class="informacion__momentos__mapa"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.670202760637!2d-74.79265042630101!3d10.988246855253674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42d65339eb257%3A0x9d19c0493a92c4fe!2sCATEDRAL%20METROPOLITANA%20MAR%C3%8DA%20REINA%20Y%20AUXILIADORA%20DE%20BARRANQUILLA!5e0!3m2!1ses-419!2sco!4v1723756831333!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
                 </div>
                 <div class="informacion__momentos__celebracion">
                     <img class="informacion__iconoImagen" src="./assets/copas.svg" alt="copas de brindis">
-                    <span>Celebración</span>
+                    <h2>Celebración</h2>
                     <span>8:00pm</span>
+                    <span class="informacion__momentos__lugar">Centro de Eventos <br>Puerta de Oro</span>
+                    <div class="informacion__momentos__mapa">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.1863572395414!2d-74.80289832630045!3d11.024640554569054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef4327f0f3fe8d5%3A0x170118464179c6bc!2sPuerta%20de%20Oro%20-%20Centro%20de%20Eventos!5e0!3m2!1ses-419!2sco!4v1723757930140!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
+            </div>
+            <div class="informacion__regalos">
+                <img class="regalos__iconoImagen" src="./assets/sobre.svg" alt="sobres de regalos">
+                <p>Tu presencia será nuestro mejor regalo, pero si deseas obsequiarnos algo, una lluvia de sobres será recibida con gratitud y alegría.</p>
+            </div>
+            <div class="informacion__vestimenta">
+                <img class="informacion__iconoImagen" src="./assets/vestimenta.svg" alt="vestido formal de mujer y traje formal de hombre">
+                <h2>Código de vestimenta</h2>
+                <span>Por definir</span>
+            </div>
+            <div class="informacion__asistencia">
+                <h2>Confirma tu asistencia</h2>
+                <form class="informacion__asistencia__formulario" action="#">
+                    <div class="asistencia__datos">
+                        <input type="text" name="nombres" placeholder="Nombres de los asistentes, separados por comas (,)">
+                        <input type="tel" name="telefono" placeholder="Teléfono">
+                    </div>
+                    <select name="asistencia" id="">
+                        <option value="" selected disabled>Confirmación</option>
+                        <option value="si">Sí, allí estaré</option>
+                        <option value="no">No</option>
+                    </select>
+                    <textarea name="deseos" id="" maxlength="300" cols="6" placeholder="Mensaje para los novios"></textarea>
+                    <input type="submit" value="Enviar confirmación">
+                </form>
             </div>
         </section>
     </main>
-    <footer></footer>
+    <footer>
+        <span class="footer__desarrollador">Desarrollado por Ronald Rodríguez Campillo</span>
+        <div class="footer__links">
+            <a class="footer__web" href="https://codews.co/">codews.co</a>
+            <a href="https://www.instagram.com/codews.co?igsh=cmRjeTNwazljcmV4"><img class="footer__iconos" src="./assets/instagram.ico" alt="logo instagram"></a>
+        </div>
+    </footer>
     <script src="./app.js"></script>
 </body>
 </html>
