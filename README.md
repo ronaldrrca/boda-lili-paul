de7c40480d3afea0a070fb6303878ea58cf343b5
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2024-08-27 17:07:44 -0500

    Agregar la seccion -informacion-

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # boda-lili-paul
 Invitación web para boda
+
+function doPost(e) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var data = JSON.parse(e.postData.contents);
+  
+  sheet.appendRow([data.nombres, data.telefono, data.asistencia, data.mensaje]);
+  
+  return ContentService.createTextOutput(JSON.stringify({ "result": "success" })).setMimeType(ContentService.MimeType.JSON);
+}
+
+
+document.getElementById('myForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var data = {
+      nombres: document.getElementById('nombres').value,
+      telefono: document.getElementById('telefono').value,
+      asistencia: document.getElementById('asistencia').value,
+      mensaje: document.getElementById('mensaje').value
+    };
+    
+    fetch('https://script.google.com/macros/s/AKfycbxXN7HggEA31IFAxapJ1dbl-Uqw21QKS3oCB_1BGl75DkNi7qwBgIfOYlF-0Yfu3Eo8/exec', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.result === 'success') {
+        alert('Data saved successfully!');
+      }
+    })
+    .catch(error => console.error('Error:', error));
+  });
+
+  <form id="myForm" class="informacion__asistencia__formulario" action="#" method="POST">
+                    <div class="asistencia__datos">
+                        <input type="text" id="nombres" name="nombres" placeholder="Nombres de los asistentes">
+                        <input type="tel" id="telefono" name="telefono" placeholder="Teléfono">
+                    </div>
+                    <select name="asistencia" id="asistencia">
+                        <option value="" selected disabled>Confirmación</option>
+                        <option value="si">Sí, allí estaré</option>
+                        <option value="no">No</option>
+                    </select>
+                    <textarea id="mensaje" maxlength="300" cols="6" name="mensaje" placeholder="Mensaje para los novios"></textarea>
+                    <input type="submit" value="Enviar confirmación">
+                </form>
---
+++ app.js
@@ -0,0 +1,119 @@
+//Variables del slider de imágenes
+let foto_slider = ['foto2.webp', 'foto3.webp', 'foto4.webp', 'foto5.webp', 'foto6.webp'];
+let indice_actual = 2;
+let detalles__slider__flechaAvanzar = document.getElementById('detalles__slider__flechaAvanzar');
+let detalles__slider__flechaRetroceder = document.getElementById('detalles__slider__flechaRetroceder'); 
+
+// Variables de la cuenta regresiva
+let detalles__cuentaRegresiva__dias = document.getElementById("detalles__cuentaRegresiva__dias");
+let detalles__cuentaRegresiva__horas = document.getElementById("detalles__cuentaRegresiva__horas");
+let detalles__cuentaRegresiva__minutos = document.getElementById("detalles__cuentaRegresiva__minutos");
+let detalles__cuentaRegresiva__segundos = document.getElementById("detalles__cuentaRegresiva__segundos");
+const fechaBoda = new Date(2025, 2, 30, 17, 0, 0);// Fecha de la boda
+
+
+//________________Código del slider de imágenes______________________________________________________________
+
+//Paso automático de las imágenes
+setInterval(function() {
+    while (indice_actual == 5) {
+            indice_actual = 0   
+    }
+    document.getElementById('detalles__slider__foto').setAttribute('src', `./assets/${foto_slider[indice_actual]}`);
+    indice_actual++;
+}, 5000)
+
+
+//Paso de las imágenes con las flechas de desplazamiento
+detalles__slider__flechaAvanzar.addEventListener('click', function(){
+    while (indice_actual == 5) {
+        indice_actual = 0   
+    }
+    document.getElementById('detalles__slider__foto').setAttribute('src', `./assets/${foto_slider[indice_actual]}`);
+    indice_actual++;
+})
+
+detalles__slider__flechaRetroceder.addEventListener('click', ()=>{
+    while (indice_actual == -1) {
+        indice_actual = 4;
+    } 
+    document.getElementById('detalles__slider__foto').setAttribute('src', `./assets/${foto_slider[indice_actual]}`);
+    indice_actual--;
+})
+
+
+//________________Código de la cuenta regresiva______________________________________________________________
+setInterval(function() { 
+    const fechaActual = new Date();
+    //Obtiene la diferencia en milisegundos
+    const diff = fechaBoda.getTime() - fechaActual.getTime();
+
+    //Convierte la diferencia a días
+    let dias = Math.floor(diff / (1000 * 60 * 60 * 24));
+    //Ajustando a doble dígito en caso de que el valor sea menor a 10
+    dias < 10 ? dias = '0' + dias : null;
+   
+    //Convierte la diferencia a horas
+    let horas = Math.floor(diff / (1000 * 60 * 60)) - (dias * 24);
+    //Ajustando a doble dígito en caso de que el valor sea menor a 10
+    horas < 10 ? horas = '0' + horas : null;
+    
+    //Convierte la diferencia a minutos
+    let minutos = Math.floor(diff / (1000 * 60)) - (dias * 24 * 60) - (horas * 60);
+    //Ajustando a doble dígito en caso de que el valor sea menor a 10
+    minutos < 10 ? minutos = '0' + minutos : null;
+    
+    //Convierte la diferencia a segundos 
+    let segundos = Math.floor(diff / 1000) - (dias * 24 * 60 * 60) - (horas * 60 * 60) - (minutos * 60);
+    //Ajustando a doble dígito en caso de que el valor sea menor a 10
+    segundos < 10 ? segundos = '0' + segundos : null;
+    
+    detalles__cuentaRegresiva__dias.innerHTML = dias;
+    detalles__cuentaRegresiva__horas.innerHTML = horas;
+    detalles__cuentaRegresiva__minutos.innerHTML = minutos;
+    detalles__cuentaRegresiva__segundos.innerHTML = segundos;
+
+}, 1000);
+
+
+
+
+
+//formulario
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxqzfZqkFGN0YMyfq32A7wt4fVQMmLx3P2lqngt-E0X09cK9hAfDGzKYhxjvEr6T3dw/exec'; 
+
+  const form = document.getElementById('myForm');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Obtener la fecha y hora actuales
+    const now = new Date();
+    
+    // Formatear la fecha y hora a "DD/MM/YYYY HH:MM:SS"
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Enero es 0
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const formattedTimestamp = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+    // Establecer el valor del campo oculto
+    document.getElementById('timestamp').value = formattedTimestamp;
+
+    //Enviar formulario 
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => response.json())
+      .then(result => console.log('Éxito:', result))
+      .catch(error => console.error('Error:', error.message));
+  });
+
+
+
+
+
+
+
